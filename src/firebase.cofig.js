5fcf080da8f55aa18481bdca0ca6e5cc5d4fe498
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-09-24 00:01:08 +0600

    project started

---
+++ src/firebase.cofig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-F9rkGpvDR-03-m46bwR42L0Uhvdj3mY",
+  authDomain: "travel-guru-dcd6f.firebaseapp.com",
+  databaseURL: "https://travel-guru-dcd6f.firebaseio.com",
+  projectId: "travel-guru-dcd6f",
+  storageBucket: "travel-guru-dcd6f.appspot.com",
+  messagingSenderId: "554767282260",
+  appId: "1:554767282260:web:fcd44d20bda4ef1b2f7dc1",
+  measurementId: "G-769GCLZV9R",
+};
+export default firebaseConfig;
