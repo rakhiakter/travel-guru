78a8c389b911ab4e01feeada0f5155614be1d853
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-09-24 21:16:14 +0600

    more code added

--- src/Components/CreateAccount/CreateAccount.js
+++ src/Components/CreateAccount/CreateAccount.js
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { Container, Form, Button } from "react-bootstrap";
 const CreateAccount = () => {
-  return <div>create an account</div>;
+  return (
+    <Container>
+      <Form id="form">
+        <h4>Create Account</h4>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="First Name" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Last Name" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Username or email" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Confirm Password" />
+        </Form.Group>
+
+        <Button variant="warning" size="lg" type="submit">
+          Create an account
+        </Button>
+        <p>
+          Already have an account? <button>Login</button>
+        </p>
+      </Form>
+    </Container>
+  );
 };
 
 export default CreateAccount;
