c698de31647cad9c5d3b3cd2f0ea19dcb548b5f2
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-09-25 21:26:05 +0600

    login almost done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,33 +1,193 @@
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import "./Login.css";
+import * as firebase from "firebase/app";
+import { useState } from "react";
+
+import "firebase/auth";
+import { useContext } from "react";
+import { UserContext } from "../../App";
+import { useHistory, useLocation } from "react-router-dom";
+import firebaseConfig from "../../../src/firebase.config";
 
 const Login = () => {
+  const history = useHistory();
+  const location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
+
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignIn: false,
+    name: "",
+    email: "",
+    password: "",
+    photo: "",
+  });
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const handleGoogleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then((res) => {
+        const { displayName, photoURL, email } = res.user;
+        const signInUser = {
+          isSignIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(signInUser);
+        setLoggedInUser(signInUser);
+        history.replace(from);
+        console.log(displayName, email, photoURL);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const handleSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then((res) => {
+        const signedOutUser = {
+          isSignIn: false,
+          name: "",
+          email: "",
+          photo: "",
+        };
+        setUser(signedOutUser);
+      })
+      .catch((err) => {
+        // An error happened.
+      });
+  };
+  const handleFbSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then(function (result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        user.success = true;
+        setUser(user);
+        setLoggedInUser(user);
+        history.replace(from);
+        // ...
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  };
+  const handleBlur = (e) => {
+    let isFieldValid = true;
+
+    if (e.target.id === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if (e.target.id === "password") {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.id] = e.target.value;
+      setUser(newUserInfo);
+    }
+  };
+  const handleSubmit = (e) => {
+    console.log("sign in clicked");
+    if (user.email && user.password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((res) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          setLoggedInUser(newUserInfo);
+          console.log(newUserInfo);
+          history.replace(from);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+          console.log(error);
+          //   updateUserName(user.name);
+        });
+    }
+
+    e.preventDefault();
+  };
   return (
     <Container>
-      <Form id="form">
+      <Form id="form" onClick={handleSubmit}>
         <h4>Login</h4>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="email">
           <Form.Label>UserName or Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onBlur={handleBlur}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+        <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onBlur={handleBlur}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Remember Me" />
           <a href="#">Forgot Password</a>
         </Form.Group>
-        <Button variant="warning" size="lg" type="submit">
-          Login
-        </Button>
+        <Button type="submit">Login</Button>
         <br />
       </Form>
+      <Button
+        variant="primary"
+        size="lg"
+        type="submit"
+        onClick={handleFbSignIn}
+      >
+        Login with Facebook
+      </Button>
+      <br />
+      <Button
+        variant="primary"
+        size="lg"
+        type="submit"
+        onClick={handleGoogleSignIn}
+      >
+        Login with Google
+      </Button>
     </Container>
   );
 };
